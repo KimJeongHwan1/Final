4f5799433f568050392eb7260357fd0ce611d225
HyangKeun Choi <gidrms99@naver.com>
2019-07-19 23:31:07 +0900

    choi0719

--- Final/src/main/java/web/controller/MemberController.java
+++ Final/src/main/java/web/controller/MemberController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
 import web.dto.Member;
@@ -34,6 +35,9 @@ public class MemberController {
 	private NaverLoginBo naverLoginBO;
 	private String apiResult = null;
 
+	// 카카오
+		private kakao_restapi kakao_restapi = new kakao_restapi();
+	
 	@Autowired
 	private void setNaverLoginBO(NaverLoginBo naverLoginBO) {
 		this.naverLoginBO = naverLoginBO;
@@ -374,7 +378,49 @@ public class MemberController {
 		} 
 
 		model.addAttribute("bool", memberService.selectImgCheck(member_code));
-	}
+	}	
 
+	
+	// 카카오 로그인
+		@RequestMapping(value = "/member/main3", produces = "application/json", method=RequestMethod.GET)
+	    public void kakaoLogin(@RequestParam("code") String code, Model model, HttpSession session) {
+	        System.out.println("로그인 할때 임시 코드값");
+	        //카카오 홈페이지에서 받은 결과 코드
+	        System.out.println(code);
+	        System.out.println("로그인 후 결과값");
+	        
+	        //카카오 rest api 객체 선언
+	        kakao_restapi kr = new kakao_restapi();
+	        //결과값을 node에 담아줌
+	        JsonNode node = kr.getAccessToken(code);
+	        //결과값 출력
+	        System.out.println(node);
+	        //노드 안에 있는 access_token값을 꺼내 문자열로 변환
+	        String token = node.get("access_token").toString();
+	        
+	        //세션에 담아준다.
+	        session.setAttribute("token", token);
+	        
+	    
+	    }
+
+		// 카카오 로그아웃
+		@RequestMapping(value = "/kakao/logout", produces = "application/json", method=RequestMethod.GET)
+	    public String Logout(HttpSession session) {
+	        //kakao restapi 객체 선언
+	        kakao_restapi kr = new kakao_restapi();
+	        //노드에 로그아웃한 결과값음 담아줌 매개변수는 세션에 잇는 token을 가져와 문자열로 변환
+	        JsonNode node = kr.Logout(session.getAttribute("token").toString());
+	        
+	        //결과 값 출력
+	        System.out.println("로그인 후 반환되는 아이디 : " + node.get("id"));
+	        
+	        return "redirect:/member/login";
+	    }   
+
+
+
+	
+	
 
 }
